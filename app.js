94da3da31e140fa228ed8c33e754b70e3bf2efdd
KristiinaCwalina <kristiina.cwalina@gmail.com>
2020-01-18 14:24:23 +0100

    array of movies

--- app.js
+++ app.js
@@ -14,10 +14,25 @@ function fetchMovies(query) {
     .then((responseBody) => showMovies(responseBody.results));
 }
 function showMovies(movies) {
+  const result = document.getElementsByClassName("result")[0];
+  result.classList.remove("hidden");
+  result.innerHTML = "";
   for (let i = 0; i < movies.length; i++) {
-    showMovie(movies[i]);
+    let movie = getMovieElement(movies[i]);
+    result.append(movie);
   }
 }
-function showMovie(movie) {
-  console.log(movie.title);
+function getMovieElement(movie) {
+  const movieElement = document.createElement("div");
+  movieElement.className = "movie";
+  movieElement.innerHTML = `
+        <img class="movie-poster" src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg">
+        <div class="movie-content">
+          <div class="movie-title">${movie.title}</div>
+          <div class="movie-overview">
+            ${movie.overview}
+          </div>
+          </div>
+   `;
+  return movieElement;
 }
