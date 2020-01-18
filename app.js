ca3e3397ae348b54005c378b44f25c9b79395cec
KristiinaCwalina <kristiina.cwalina@gmail.com>
2020-01-18 13:37:22 +0100

    functions added

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const apiKey = "3ddca7ee7a434ea70bc38f8560648498";
+const baseUrl = "https://api.themoviedb.org/3";
 let searchBox = document.getElementById("searchbox");
 searchBox.addEventListener("keyup", onSearch);
 function onSearch(event) {
   if (event.keyCode == 13) {
-    console.log(searchBox.value);
+    fetchMovies(searchBox.value);
   }
+}
+function fetchMovies(query) {
+  const url = `${baseUrl}/search/movie?api_key=${apiKey}&query=${query}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseBody) => showMovies(responseBody.results));
+}
+function showMovies(movies) {
+  for (let i = 0; i < movies.length; i++) {
+    showMovie(movies[i]);
+  }
+}
+function showMovie(movie) {
+  console.log(movie.title);
 }
